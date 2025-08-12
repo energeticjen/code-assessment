955a5cb06bb994da4ed38f0dc4fd7dfa75467be7
Paschke, Jenny <jenny.paschke@jpmchase.com>
2025-08-12 13:30:38 -0400

    Added tests

--- README.md
+++ README.md
@@ -5,13 +5,12 @@ Developer: Jennifer Paschke
 jennifer.lh.paschke@gmail.com
 
 1. Set up virtual environment.
-
-2. Start it by typing .venv/scripts/activate in the terminal.
-
-3. The only things needed to be installed are python and django. 
-
-4. In the terminal, type: pip install django (if it isn't already installed).
-
-5. Then run: python manage.py runserver
+2. cd into codeAssessment directory.
+3. Start venv by typing .venv/scripts/activate in the terminal.
+4. The only things needed to be installed is django.
+5. In the terminal, type: pip install django (if it isn't already installed).
+6. Then run: python manage.py runserver
 
 The main page is at 127.0.0.1/signup
+
+To test, run: python manage.py test
--- codeAssessment/signup_app/tests.py
+++ codeAssessment/signup_app/tests.py
@@ -1,3 +1,45 @@
-from django.test import TestCase
+from django.test import Client, TestCase
 
-# I normally do tests, but I ran out of time.
+from .forms import UserForm
+from .models import User
+
+client = Client()
+
+class UserModelTests(TestCase):
+
+    def test__user_string_representation(self):
+        user = User.objects.create(name="test", title="test_title")
+        assert str(user) == "test"
+
+
+class UserFormTests(TestCase):
+
+    def test__user_form_requires_name(self):
+        data = {"name": "", "title": "test_title"}
+        form = UserForm(data=data)
+        self.assertFalse(form.is_valid())
+
+    def test__user_form_requires_title(self):
+        data = {"name": "test", "title": ""}
+        form = UserForm(data=data)
+        self.assertFalse(form.is_valid())
+
+
+class UserViewTests(TestCase):
+
+    def test__view_homepage_response(self):
+        response = client.get('/signup/')
+        assert response.status_code == 200
+        self.assertTemplateUsed(response, 'signup/home.html')
+
+    def test__data_saved(self):
+        data = {"name": "test", "title": "test_title"}
+        response = client.post('/signup/submit/', data=data, follow=True)
+        self.assertTemplateUsed(response, "signup/confirm.html")
+        user = User.objects.get(title="test_title")
+        assert user.name == "test"
+
+    def test__view_confirm(self):
+        User.objects.create(name="test", title="test_title", age=20, hometown="test_town")
+        response = client.get('/signup/confirm/')
+        self.assertContains(response, '<td>test</td>')
